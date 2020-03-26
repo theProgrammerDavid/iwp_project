057e874a66e6817edba5b76317497334c3a60551
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-03-26 16:26:01 +0530

    feat: api route and static folder

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 
 const express = require('express')
 const app = express()
+
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.use('/api', require('./routes/api.js'));
+app.use(express.static('static'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
