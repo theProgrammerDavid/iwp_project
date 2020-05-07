dfa9c7ca736304dd3ac3e554aca309d2bab3e162
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-05-07 15:17:02 +0530

    fix: merge error

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -3,6 +3,8 @@ const express = require('express')
 const bcrypt = require('bcrypt');
 const router = express.Router()
 const hashPassword = require('../util/hash')['hashPassword'];
+ 
+
 
 router.get('/', (req, res) => {
     res.render('login', { layout: 'layout/beforeSignIn' });
@@ -10,9 +12,11 @@ router.get('/', (req, res) => {
 
 
 router.post('/', async function(req, res){
-    //console.log(req.body.password);
-    //const hash = await bcrypt.hash(req.body.password,10).exec();
+
+    // console.log(req.body.password);
+    console.log(await hashPassword(req.body.password));
     res.send('ok');
+
 });
 
 module.exports = router;
