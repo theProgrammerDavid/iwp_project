690454dd7762867a507a0b429099261160ca0939
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-05-12 02:48:29 +0530

    fix: GET -> POST

--- static/js/test.js
+++ static/js/test.js
@@ -1,21 +1,17 @@
 $(document).ready(function () {
 
     document.addEventListener("visibilitychange", function (e) {
-        alert('Cheating Detected. Exam will end');
+
         const Url = window.location.href + '/event';
-        console.log('url is : ' + Url);
-        $.ajax({
-            url: Url,
-            type: "GET",
-            success: function (result) {
-                console.log('result is : ' + result);
-                window.location.replace("http://localhost:3000/home")
+
+        $.post(Url,
+            {
+                event: 'window change'
             },
-            error: function (err) {
-                console.log('error is');
-                console.log(err);
-            }
-        });
+            function (data, status) {
+                alert("Caught Cheating. Test will end");
+                window.location.replace("http://localhost:3000/home")
+            });
     });
 });
 
