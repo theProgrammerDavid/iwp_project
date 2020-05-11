0843a7f284f9822dbf424512faac12b4d0cba7fb
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-05-12 02:34:46 +0530

    feat: Anti Cheat

--- static/js/test.js
+++ static/js/test.js
@@ -1,36 +1,22 @@
+$(document).ready(function () {
 
-window.addEventListener("focus", function(event) 
-{ 
-    $.get("/event", function(data, status){
-        alert("Cheating detected. Test will end");
-      });
-}, false);
+    document.addEventListener("visibilitychange", function (e) {
+        alert('Cheating Detected. Exam will end');
+        const Url = window.location.href + '/event';
+        console.log('url is : ' + Url);
+        $.ajax({
+            url: Url,
+            type: "GET",
+            success: function (result) {
+                console.log('result is : ' + result);
+                window.location.replace("http://localhost:3000/home")
+            },
+            error: function (err) {
+                console.log('error is');
+                console.log(err);
+            }
+        });
+    });
+});
 
-// Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get today's date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="demo"
-  document.getElementById("countdown-timer").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);
