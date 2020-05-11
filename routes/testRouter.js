62f3825f0759c9bb921111e5835028df73864de9
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-05-11 23:57:21 +0530

    [WIP] Test page

--- routes/testRouter.js
+++ routes/testRouter.js
@@ -5,9 +5,13 @@ const router = express.Router()
 
 router.post('/', async function (req, res) {
 
-    
-    res.send('test page');
+    res.render('testpage', { layout: 'layout/afterSignIn' });
+    //res.send('test page: '+req.body.testid);
 
+});
+
+router.post('/event', async function (req, res) {
+    res.redirect('/home');
 });
 
 module.exports = router;
