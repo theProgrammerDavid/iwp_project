21d6b147279023f0857364e313130129463baeac
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-05-13 02:14:55 +0530

    feat: Check if Test exists, Question Adder

--- routes/testRouter.js
+++ routes/testRouter.js
@@ -1,12 +1,48 @@
 
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose');
 const Offense = require('../models/Offense');
+const Question = require('../models/Question');
+
+
+
+router.post('/make', async function (req, res) {
+    xx.forEach(function (d) {
+        let q = new Question({
+            "Correct Answer": d["Correct Answer"],
+            "Option 1": d["Option 1"],
+            "Option 2": d["Option 2"],
+            "Option 3": d["Option 3"],
+            "Option 4": d["Option 4"],
+            "Question": d["Question"],
+            testid: 1,
+        });
+
+        q.save()
+            .then(doc => {
+                console.log(doc)
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    });
+    res.send('ok');
+})
 
 router.post('/', async function (req, res) {
+    console.log(req.body.testid);
+    mongoose.model('Question').findOne({ testid: req.body.testid }, function (err, doc) {
+        if (doc) {
+            res.render('testpage', { layout: 'layout/afterSignIn' });
+        }
+        else {
 
-    res.render('testpage', { layout: 'layout/afterSignIn' });
-    //res.send('test page: '+req.body.testid);
+            res.send('test not found');
+        }
+    })
+
+
 
 });
 
@@ -14,7 +50,7 @@ router.post('/event', async function (req, res) {
     console.log('received cheating reuqest on ' + req.session.email);
     let offense = new Offense({ email: req.session.email });
     offense.save().then().
-        catch((err) => { console.err(err) });
+        catch((err) => { console.log(err) });
 
     res.send('confirmed');
 });
