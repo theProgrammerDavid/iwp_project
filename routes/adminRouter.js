d4cf30bf7d791329ce76a31924e90a3baff3c601
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-05-13 23:09:48 +0530

    [WIP] Admin Panel: Question

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -2,25 +2,39 @@
 const express = require('express')
 const bcrypt = require('bcrypt');
 const router = express.Router()
+const Question = require('../models/Question');
+const User = require('../models/User');
 const hashPassword = require('../util/hash')['hashPassword'];
 
-const adminPass = "test123";
+const adminPass = 'admin';
 
 router.get('/', function (req, res) {
     if (req.session.username === 'admin')
-        res.send('admin panel login')
+        res.render('adminHome', { layout: 'layout/beforeAdminLogin' });
     // res.render('adminLogin', { layout: 'layout/beforeAdminLogin' });
     else res.redirect('/admin/login');
 })
 
-
+router.post('/add/question', function (req, res) {
+    let q = new Question({
+        "Question": req.body.question,
+        "Option 1": req.body.option1,
+        "Option 2": req.body.option2,
+        "Option 3": req.body.option3,
+        "Option 4": req.body.option4,
+        testid: req.body.testid,
+        "Correct Answer":req.body.correctAnswer
+    });
+});
 
 router.get('/login', function (req, res) {
     res.render('adminLogin', { layout: 'layout/beforeAdminLogin' });
 })
 router.post('/login', function (req, res) {
-    if (req.body.username === 'admin' && req.body.password === 'password') {
-        res.render('adminHome', { layout: 'layout/beforeAdminLogin' });
+    console.log(req.body)
+    if (req.body.username === 'admin' && req.body.password === adminPass) {
+        req.session.username = 'admin';
+        res.redirect('/admin');
     }
     else
         res.render('adminLogin', { layout: 'layout/beforeAdminLogin', msg: 'Invalid Credentials' });
