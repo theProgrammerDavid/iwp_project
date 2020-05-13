aff124cf66d73b757f9bfcc588c9389175cb747b
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-05-14 02:16:54 +0530

    fix: route clasing

--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -1,6 +1,8 @@
 
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose');
+const Question = require('../models/Question');
 const testRouter = require('./testRouter');
 
 router.get('/', (req, res) => {
@@ -10,4 +12,20 @@ router.get('/', (req, res) => {
 
 router.use('/test', testRouter);
 
+router.post('/testid', async function (req, res) {
+    console.log(req.body.testid);
+    mongoose.model('Question').findOne({ testid: req.body.testid }, function (err, doc) {
+        if (doc) {
+            //rand = randomNumberGenerator()
+            res.redirect('/home/test');
+        }
+        else {
+
+            res.send('test not found');
+        }
+    })
+
+
+
+});
 module.exports = router;
