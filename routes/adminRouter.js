2c5aa20d4939e18dfb21a5a0ba3bc3b8a571873f
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-05-13 16:47:56 +0530

    [WIP]: Admin Panel: Route

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -6,9 +6,23 @@ const hashPassword = require('../util/hash')['hashPassword'];
 
 const adminPass = "test123";
 
-router.get('/', function (req,res){
-    res.render('adminLogin', { layout: 'layout/beforeAdminLogin' });
+router.get('/', function (req, res) {
+    if (req.session.username === 'admin')
+        res.send('admin panel login')
+    // res.render('adminLogin', { layout: 'layout/beforeAdminLogin' });
+    else res.redirect('/admin/login');
 })
 
 
+
+router.get('/login', function (req, res) {
+    res.render('adminLogin', { layout: 'layout/beforeAdminLogin' });
+})
+router.post('/login', function (req, res) {
+    if (req.body.username === 'admin' && req.body.password === 'password') {
+        res.render('adminHome', { layout: 'layout/beforeAdminLogin' });
+    }
+    else
+        res.render('adminLogin', { layout: 'layout/beforeAdminLogin', msg: 'Invalid Credentials' });
+})
 module.exports = router;
