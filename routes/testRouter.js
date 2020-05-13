6c90b9116ba5893261eab24fc9ebe880600dfc2e
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-05-14 01:10:03 +0530

    fix: JSON parsing

--- routes/testRouter.js
+++ routes/testRouter.js
@@ -6,16 +6,21 @@ const Offense = require('../models/Offense');
 const Question = require('../models/Question');
 const User = require('../models/User')
 
-function randomNumberGenerator(){
+function randomNumberGenerator() {
     var x = Math.floor((Math.random() * 20) + 1);
     return x
 }
 router.get('/', (req, res) => {
+    console.log('qss');
     rand = randomNumberGenerator()
-    Question.find({"Serial Number": rand}).then((question) => {
-        console.log(question)
-        res.render("testpage", {layout: 'layout/afterSignIn', question: question})
-    }).catch((e)=>{
+    Question.findOne({ "Serial Number": rand.toString() }).then((q) => {
+        console.log('here');
+        console.log(q)
+        res.render("testpage", {
+            layout: 'layout/afterSignIn',
+            op1: q['Option 1'], op2: q["Option 2"], op3: q["Option 3"], op4: q["Option 4"]
+        })
+    }).catch((e) => {
         res.status(500)
     })
 })
