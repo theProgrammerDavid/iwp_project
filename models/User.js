b8f705a525d3a6a6af23327e7e10c246bfd4cbba
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-05-15 20:18:25 +0530

    formatting: User model

--- models/User.js
+++ models/User.js
@@ -4,35 +4,35 @@ let validator = require('validator')
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-		Name: {
-			type: String,
-			required: true,
-			select: true,
-		},
-		Email: {
-			type: String,
-			required: true,
-			select: true,
-			
-			lowercase: true,
-			validate : (value)=>{
-				return validator.isEmail(value);
-			}
-		},
-		Phone: {
-			type: String,
-			required: true,
-			select: true,
-		},
-		Password: {
-			type: String,
-			required: true,
-			select: true,
-		},
-		Timer:{
-			type: Number,
-			default: 60*60,
+	Name: {
+		type: String,
+		required: true,
+		select: true,
+	},
+	Email: {
+		type: String,
+		required: true,
+		select: true,
+
+		lowercase: true,
+		validate: (value) => {
+			return validator.isEmail(value);
 		}
+	},
+	Phone: {
+		type: String,
+		required: true,
+		select: true,
+	},
+	Password: {
+		type: String,
+		required: true,
+		select: true,
+	},
+	Timer: {
+		type: Number,
+		default: 60 * 60,
+	}
 
 });
 
